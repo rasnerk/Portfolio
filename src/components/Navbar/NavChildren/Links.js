516aa07d48bb9b0be770fb7630b1c4de8aeb2da5
Keifer Rasner <rasnerkeifer@gmail.com>
2022-07-12 10:22:24 -0500

    Navbar Slide

--- src/components/Navbar/NavChildren/Links.js
+++ src/components/Navbar/NavChildren/Links.js
@@ -5,33 +5,42 @@ const Links = () => {
     const scroll = Scroll.animateScroll;
     const scroller = Scroll.scroller;
     const [width, setWidth] = useState(window.innerWidth)
-    const [displayMenu, setDisplayMenu] = useState(false)
 
+    const menuSlide = () => {
+        let menu = document.querySelector("#menu")
+        if (menu.classList.contains('slideIn')) {
+            document.querySelector("#menu").classList.remove('slideIn')
+            document.querySelector("#menu").classList.add('slideOut')
+        } else {
+            document.querySelector("#menu").classList.remove('slideOut')
+            document.querySelector("#menu").classList.add('slideIn')
+        }
+    }
     useEffect(() => {
         const handleWindowResize = () => setWidth(window.innerWidth)
         window.addEventListener("resize", handleWindowResize);
         
         return () => window.removeEventListener("resize", handleWindowResize);
     }, [])
-
+    
     return (
         <div className="navbar-links col-xxl-4 col-lg-7 col-md-10 col-12">
             {width <= 768 &&
-                <img onClick={() => setDisplayMenu(prev => !prev)} src='imgs/menu.png' alt='menu' width={"50px"} height={"50px"} id="menu-img" />
+            <>
+                <img onClick={menuSlide} src='imgs/menu.png' alt='menu' width={"50px"} height={"50px"} id="menu-img" />
+                <ul id='menu'>
+                    <li onClick={() => { menuSlide(); scroll.scrollToTop({duration: 100, smooth: true})} }>Home</li>
+                    <li onClick={() => { menuSlide(); scroller.scrollTo('view-projects', { spy: true, smooth: true, duration: 100 }) }}>Projects</li>
+                    <li onClick={() => { menuSlide(); scroller.scrollTo('contact', { spy: true, smooth: true, duration: 100 })} }>Contact Me</li>
+                </ul>
+            </>
             }
-            {width >= 768 ?
+            {width >= 768 &&
                 <ul id='menu'>
                     <li onClick={() => scroll.scrollToTop({duration: 100, smooth: true})}>Home</li>
                     <li onClick={() => scroller.scrollTo('view-projects', { spy: true, smooth: true, duration: 100 })}>Projects</li>
                     <li onClick={() => scroller.scrollTo('contact', { spy: true, smooth: true, duration: 100 })}>Contact Me</li>
-                </ul>
-                :
-                displayMenu && 
-                <ul id='menu'>
-                    <li onClick={() => { setDisplayMenu(prev => !prev); scroll.scrollToTop({duration: 100, smooth: true})} }>Home</li>
-                    <li onClick={() => { setDisplayMenu(prev => !prev); scroller.scrollTo('view-projects', { spy: true, smooth: true, duration: 100 }) }}>Projects</li>
-                    <li onClick={() => { setDisplayMenu(prev => !prev); scroller.scrollTo('contact', { spy: true, smooth: true, duration: 100 })} }>Contact Me</li>
-                </ul>
+                </ul> 
             }
       </div>
     )
